42a9da46dc429ccd453a5a8bab03e5ee334e099e
Pranshu Khanna <khanna.pranshu@yahoo.com>
2022-05-09 22:38:10 +0200

    Redirect Fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
-    {
-        "redirects": [
-        { 
-            "source": "/hello-vercel",
-            "destination": "https://vercel.com/", 
-            "permanent": true
-        }
-        ]
-    }
