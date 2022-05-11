381b0be2cb142b02b84e8a42a3817f3af698cd86
Pranshu Khanna <khanna.pranshu@yahoo.com>
2022-05-11 14:37:12 +0200

    Fixing OG Slash

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   const meta = {
     title: 'Pranshu Khanna',
     description: 'Website metas are so meta.',
-    image: 'public/images/pransh-og-min.png'
+    image: '/public/images/pransh-og-min.png'
   }
 
   return (
