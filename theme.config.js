757fb5298e96cdaf955c7dffcb07a6ff79d30f0c
Pranshu Khanna <khanna.pranshu@yahoo.com>
2022-07-02 00:51:45 +0200

    Updated OG location.

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Pranshu Khanna, whatever that means
+      <time>{YEAR}</time> © Pranshu Khanna, whatever that means.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
